0cefb91be74507305f51fbff62b719edd020e898
Nic Feanny <feannyn@gmail.com>
2019-01-01 10:01:21 -0500

    Initial Commit

---
+++ Calculator Stack View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator Stack View
+//
+//  Created by Nicholas Feanny on 1/1/19.
+//  Copyright © 2019 Nicholas Feanny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
